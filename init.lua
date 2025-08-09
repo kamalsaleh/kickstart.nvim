1c3c5d1748f7a25680948ea53f10cb811b0e732d
Kamal Saleh <kamal.saleh@web.de>
2025-08-09 11:27:57 +0200

    install NeoTerm to attach to each buffer a terminal

--- init.lua
+++ init.lua
@@ -983,7 +983,26 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  -- terminal-buffer for each buffer.
+  {
+    'nyngwang/NeoTerm.lua',
+    config = function()
+      require('neo-term').setup {
+        exclude_filetypes = { 'oil' },
+        -- exclude_buftypes = {}, -- 'terminal' will always be added by NeoTerm.lua
+        -- enabled by default!
+        -- presets = {
+        --   'vim-test',
+        -- }
+      }
+      vim.keymap.set('n', '<M-Tab>', function()
+        vim.cmd 'NeoTermToggle'
+      end)
+      vim.keymap.set('t', '<M-Tab>', function()
+        vim.cmd 'NeoTermEnterNormal'
+      end)
+    end,
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
