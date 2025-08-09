bc3db1dc5b8f195fb72d40682416e6d9a325ec3d
Kamal Saleh <kamal.saleh@web.de>
2025-08-09 11:30:07 +0200

    install 'vim-visual-multi' plugin for multiple selection

--- init.lua
+++ init.lua
@@ -1003,6 +1003,11 @@ require('lazy').setup({
       end)
     end,
   },
+  -- Multiple selection
+  {
+    'mg979/vim-visual-multi',
+    branch = 'master',
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
